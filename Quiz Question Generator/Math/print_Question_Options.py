bb4a002a8a07e16c8479803b468ef2776e1b6b9b
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-07-26 22:36:57 +0530

    Added a argument operator to specify the operation

--- Quiz Question Generator/Math/print_Question_Options.py
+++ Quiz Question Generator/Math/print_Question_Options.py
@@ -1,7 +1,7 @@
-def print_Ques_Opt_Ans(terms,options,number_of_options,answer):
-    print("Select the correct option for the fraction multiplication : ")
+def print_Ques_Opt_Ans(terms,options,number_of_options,answer,operator):
+    print("Select the correct option for the following multiplication : ")
     for term in terms[0:-1]:
-        print(term,end=" * ")
+        print(term," ",operator,end=" ")
     print(terms[-1],"  = ")
     print("The options are given below : ")
     for i in range(0,number_of_options):
