54bbec72c9d8bed06071e7a4e4582c723beccfdd
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-07-27 12:10:40 +0530

    Create generate_Latex_Format.py

---
+++ Quiz Question Generator/Math/generate_Latex_Format.py
@@ -0,0 +1,9 @@
+'''
+Author : Bhathrinaathan M B
+'''
+def operation_of_the_operator(operator):
+    return "Add"
+
+def format_complex_number_with_operation(terms,options,number_of_options,answer,operator):
+    format=""
+    operation=operation_of_the_operator(operator)    
