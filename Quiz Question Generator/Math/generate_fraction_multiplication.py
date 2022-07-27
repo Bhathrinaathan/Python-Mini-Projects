69b9d904a2bbd6e43da0bca872eebd27578b992b
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-07-27 12:07:52 +0530

    Added the compatability for latex formatting

--- Quiz Question Generator/Math/generate_fraction_multiplication.py
+++ Quiz Question Generator/Math/generate_fraction_multiplication.py
@@ -1,12 +1,15 @@
-import random as rand 
-from fractions import Fraction as frac
-import numpy
-import print_Question_Options as display
+import random as rand   #To generate random numbers
+from fractions import Fraction as frac  #To use the fractions 
+import numpy    #To calculate the product of complex numbers
+import print_Question_Options as display    #Display the questions , options and answer
+import generate_Latex_Format as latex      #To format the output in latex format
 
+#Genrates a random number for the given complexity
 def generate_number(complexity):
     bound={1:(1,5), 2:(5,25), 3:(10,50), 4:(15,100), 5:(20,200)}
     return (rand.randint(bound[complexity][0],bound[complexity][1]))
 
+#Genrates a complex number using randint function
 def generate_fraction(complexity):
     return (frac(generate_number(complexity),generate_number(complexity)))
 
@@ -22,6 +25,7 @@ def generate_question(complexity,number_of_options):
     if product not in options[:-1:]:
         options[number_of_options-1]=product
     display.print_Ques_Opt_Ans(terms,options,number_of_options,product,'*')
+    
 
 if __name__=='__main__':
     generate_question(4,4)
