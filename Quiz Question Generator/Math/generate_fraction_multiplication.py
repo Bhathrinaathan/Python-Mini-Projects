8965c96b8256040de1fd043e8fcd18c2a6df87bd
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-07-27 17:15:04 +0530

    Update generate_fraction_multiplication.py

--- Quiz Question Generator/Math/generate_fraction_multiplication.py
+++ Quiz Question Generator/Math/generate_fraction_multiplication.py
@@ -4,6 +4,7 @@ import numpy    #To calculate the product of complex numbers
 import print_Question_Options as display    #Display the questions , options and answer
 import generate_Latex_Format as latex      #To format the output in latex format
 
+
 #Genrates a random number for the given complexity
 def generate_number(complexity):
     bound={1:(1,5), 2:(5,25), 3:(10,50), 4:(15,100), 5:(20,200)}
@@ -25,7 +26,10 @@ def generate_question(complexity,number_of_options):
     if product not in options[:-1:]:
         options[number_of_options-1]=product
     display.print_Ques_Opt_Ans(terms,options,number_of_options,product,'*')
+    ques_latex,options_latex,answer_latex=latex.format_question_fraction_number(terms,options[:number_of_options],product,'*')
+    print(ques_latex,"\n",options_latex,"\n",answer_latex)
+    for i in options_latex:
+        print(i,end=",,,")
     
-
 if __name__=='__main__':
     generate_question(4,4)
