a4eba092909022a3733eb3e6428253c41d1ba6be
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-07-29 12:50:41 +0530

    Update generate_fraction_multiplication.py

--- Quiz Question Generator/Math/generate_fraction_multiplication.py
+++ Quiz Question Generator/Math/generate_fraction_multiplication.py
@@ -3,8 +3,14 @@ from fractions import Fraction as frac  #To use the fractions
 import numpy    #To calculate the product of complex numbers
 import print_Question_Options as display    #Display the questions , options and answer
 import generate_Latex_Format as latex      #To format the output in latex format
+import JSON_Formatters
+import json
 
-
+author_Name='Bhathrinaathan M B'
+guid_Id=''
+sys_Id='Hexint09'
+reveiwer_Name=''
+reveiwer_Id=''
 #Genrates a random number for the given complexity
 def generate_number(complexity):
     bound={1:(1,5), 2:(5,25), 3:(10,50), 4:(15,100), 5:(20,200)}
@@ -27,9 +33,9 @@ def generate_question(complexity,number_of_options):
         options[number_of_options-1]=product
     display.print_Ques_Opt_Ans(terms,options,number_of_options,product,'*')
     ques_latex,options_latex,answer_latex=latex.format_question_fraction_number(terms,options[:number_of_options],product,'*')
-    print(ques_latex,"\n",options_latex,"\n",answer_latex)
-    for i in options_latex:
-        print(i,end=",,,")
-    
+    data=JSON_Formatters.format_json_file(ques_latex,options_latex,answer_latex,author_Name,guid_Id,sys_Id,reveiwer_Name,reveiwer_Id)
+    json.dumps(data)
+
+
 if __name__=='__main__':
     generate_question(4,4)
